5bef628cb39c2f71432bcd259ea69bacbccaf0ad
HasrilMuhammadIbal <113493778+HasrilMuhammadIbal@users.noreply.github.com>
2022-09-20 18:45:49 +0700

    Add files via upload
    jdk-18.0.2.1
    Spring Boot 2.7.3
    Packaging jar

---
+++ Latihan-service/Latihan-service/src/test/java/com/Hasril/Latihan/service/LatihanServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Hasril.Latihan.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LatihanServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
